1d2a0f1f15d678b9bc40ad00c9ed880a0831334e
Reneta <renkagh55@gmail.com>
2021-11-09 09:06:34 +0200

    --

--- Asynchronous Programming/Lab/Cookbook/app.js
+++ Asynchronous Programming/Lab/Cookbook/app.js
@@ -10,7 +10,7 @@ fetch(url)
       let currentRecipe = recipes[key];
       console.log(currentRecipe);
       //closed recipe elements
-      // let id = currentRecipe._id;
+      let id = currentRecipe._id;
       // let image = currentRecipe.img;
       let name = currentRecipe.name;
 
@@ -41,7 +41,61 @@ fetch(url)
       articleEl.addEventListener("click", showRecipe);
 
       function showRecipe(e) {
-        //todo
+        let articleElement = e.currentTarget;
+        console.log(articleElement);
+
+        let divTitle = articleElement.querySelector(".title");
+        let heading = articleElement.querySelector(".title h2");
+        // articleElement.appendChild(heading);
+        // divTitle.remove();
+
+        let bandDiv = document.createElement("div");
+        bandDiv.className = "band";
+
+        let divElement = document.createElement("div");
+        divElement.className = "ingredients";
+
+        let imgDiv = articleElement.querySelector(".small");
+        imgDiv.className = "thumb";
+        bandDiv.appendChild(imgDiv);
+
+        let ingrdElement = document.createElement("h3");
+        ingrdElement.textContent = "Ingredients:";
+
+        let ulElement = document.createElement("ul");
+        //let liElement = document.createElement("li");
+
+        let descriptionElement = document.createElement("div");
+        descriptionElement.className = "description";
+        let prepElement = document.createElement("h3");
+        prepElement.textContent = "Preparation";
+
+        //let pElement = document.createElement("p");
+
+        descriptionElement.appendChild(prepElement);
+
+        fetch(`${url}/details/${id}`)
+          .then((response) => response.json())
+          .then((rec) => {
+            console.log(rec);
+            let array = rec.ingredients;
+            array.forEach((e) => {
+              let liElement = document.createElement("li");
+              liElement.textContent = e;
+              ulElement.appendChild(liElement);
+            });
+            let prep = rec.steps;
+            prep.forEach((e) => {
+              let paragraph = document.createElement("p");
+              paragraph.textContent = e;
+              descriptionElement.appendChild(paragraph);
+            });
+          });
+
+        divElement.appendChild(liElement);
+        bandDiv.appendChild(divElement);
+        articleElement.appendChild(bandDiv);
+        articleElement.appendChild(descriptionElement);
       }
     }
   });
