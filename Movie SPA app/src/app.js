775f80cf3db24af1f7fab2346ecf33822a608c44
Reneta <renkagh55@gmail.com>
2021-11-11 14:30:00 +0200

    middleware

--- Movie SPA app/src/app.js
+++ Movie SPA app/src/app.js
@@ -1,10 +1,10 @@
-import { render, html } from "../node_modules/lit-html/lit-html.js";
 import page from "../node_modules/page/page.mjs";
+import { renderMiddleware } from "./middlewares/renderMiddleware.js";
 
 import { homePage } from "./views/homeView.js";
 import { loginPage } from "./views/loginView.js";
 import { moviePage } from "./views/movieView.js";
-
+page(renderMiddleware);
 page("/", homePage);
 page("/movies", moviePage);
 page("/login", loginPage);
